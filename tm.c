9e0bfacbe4a899cfc1eaca1e88538cde3146b08e
sakshi-kinge <sakshikinge66@gmail.com>
2026-09-02 14:24:37 +0530

    Add files via upload

---
+++ tm.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char tape[100];
+	int left,right;
+	int palindrome=1;
+
+	printf("Enter a string:");
+	scanf("%99s",tape);
+	left=0;
+	right=strlen(tape)-1;
+	while(left<right)
+	{
+		if(tape[left]!=tape[right])
+		{
+			palindrome=0;
+			break;
+		}
+		left++;
+		right--;
+	}
+	if(palindrome)
+		printf("String Accepted(Palindrome)");
+	else
+		printf("String Rejected(Not Palindrome");
+	return 0;
+}
+
+
